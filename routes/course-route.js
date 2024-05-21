3b0e50378b98aea2067f42dc008aba742a57a0ca
你的用户名 <ryder21687592@gmail.com>
2024-05-21 23:11:48 +0800

    change6

--- routes/course-route.js
+++ routes/course-route.js
@@ -69,7 +69,7 @@ router.get("/:_id", async (req, res) => {
 const saveImage = (base64String) => {
   const base64Data = base64String.replace(/^data:image\/\w+;base64,/, "");
   const imageData = Buffer.from(base64Data, "base64");
-  const imagePath = path.join(__dirname, `/img/image-7.jpg`);
+  const imagePath = path.join(__dirname, `client/public/img/image-7.jpg`);
   fs.writeFileSync(imagePath, imageData);
   return `http://your-app-url/${imagePath}`;
 };
